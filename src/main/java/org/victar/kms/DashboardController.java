ca30a419699f7ab05da1163efa874d1f5c3e9afe
Dennis Vysotskiy <Dennis.Vysotskiy@gmail.com>
2016-06-05 09:30:51 -0700

    DV: updated dashboard

--- src/main/java/org/victar/kms/DashboardController.java
+++ src/main/java/org/victar/kms/DashboardController.java
@@ -6,27 +6,33 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.victar.kms.dal.KeyRepository;
+import org.victar.kms.dal.*;
 
 @Controller
 public class DashboardController {
+	
+	@Autowired
+	KeyRepository keyRepository;
 
 	private static final Logger logger = LoggerFactory.getLogger(DashboardController.class);
 
 	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
 	public String keys(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "dashboard";
 	}
 }
